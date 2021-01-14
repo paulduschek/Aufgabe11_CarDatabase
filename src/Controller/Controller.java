129c5ece18d2d27cfd3aa72036ff0645084e1609
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2021-01-14 12:21:41 +0100

    implemented methods initializeDB, addVehicle in CarDatabase.java, connected Controller.java to sample.fxml

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -5,5 +5,29 @@
 
 package Controller;
 
-public class Controller {
+import Model.CarDatabase;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    //Variable Definitions
+    private CarDatabase cdB;
+    private Stage stage;
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private TextArea resultArea;
 }
--- src/Model/CarDatabase.java
+++ src/Model/CarDatabase.java
@@ -1,9 +1,28 @@
 package Model;
 
+import java.util.HashMap;
+
 /**
  * @author Paul Duschek
  * @version 1.0, 7.1.2021
  */
 
 public class CarDatabase {
+
+    private HashMap<String, Vehicle> db = new HashMap();
+
+    public void addVehicle()
+    {
+        String ma = MagicGenerator.getRandomManufacturer();
+        Vehicle v = new Vehicle(MagicGenerator.getRandomColor(), MagicGenerator.getRandomName(), ma, MagicGenerator.getRandomModel(ma), MagicGenerator.getRandomLicencePlate());
+        db.put(v.getLicensePlate(), v);
+    }
+
+    public void initializeDB(){
+        System.out.println("Test1");
+        for (int i = 0; i <= 1000; i++) {
+            addVehicle();
+            System.out.println("Test2");
+        }
+    }
 }
