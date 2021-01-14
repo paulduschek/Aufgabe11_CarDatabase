4e198d00f9339c220f59958a8bca4a6f3689a020
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2021-01-14 12:29:50 +0100

    implemented show method in Controller.java

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -7,7 +7,10 @@ package Controller;
 
 import Model.CarDatabase;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -30,4 +33,31 @@ public class Controller implements Initializable {
 
     @FXML
     private TextArea resultArea;
+
+    //setter
+    public void setStage(Stage s) {
+        this.stage = s;
+    }
+
+    public void defineDB() {
+        cdB.initializeDB();
+    }
+
+    public static void show(Stage stage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(Controller.class.getResource("sample.fxml"));
+            Parent root = loader.load();
+
+            Controller c = loader.getController();
+            c.setStage(stage);
+            c.defineDB();
+
+            stage.setTitle("Duschek Aufgabe 11 - CarDatabase");
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+        catch (Exception ex) {
+            System.out.println("An error occured.");
+        }
+    }
 }
