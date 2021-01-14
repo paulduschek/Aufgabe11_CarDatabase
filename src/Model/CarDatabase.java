38531bfc9c35fc8e8a1dd0b7304b37e3682a970c
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2021-01-14 11:01:18 +0100

    implemented class MagicGenerator, renamed class Vehicle.java, created class CarDatabase.java

---
+++ src/Model/CarDatabase.java
@@ -0,0 +1,9 @@
+package Model;
+
+/**
+ * @author Paul Duschek
+ * @version 1.0, 7.1.2021
+ */
+
+public class CarDatabase {
+}
--- src/Model/Vehicle.java
+++ src/Model/Vehicle.java
@@ -11,7 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Vehicle extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
